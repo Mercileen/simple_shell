3cc4c94192dd5fcffa6da60220c8b3ac4fbf5825
Mercileen <kamwindamercy@gmail.com>
2023-11-10 01:11:39 -0800

    simple

---
+++ welcome.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A  program that printing welcome home
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("welcome home\n");
+	return (0);
+}
